5d545ef4671551f7809fb5add6d59d3f216e5032
agata <agata@nulab.co.jp>
2011-08-19 02:03:06 +0900

    add comment js/inline_01.js

--- twitter.com/formatted/js/inline_01.js
+++ twitter.com/formatted/js/inline_01.js
@@ -1,10 +1,13 @@
   document.domain = 'twitter.com';
 
   // this will be copied to twttr.appStartTime once our JS has started up
+  /// 開始時間の設定
   document.startTime = new Date().getTime();
+  /// グローバルオブジェクト twttr
   var twttr = {};
   twttr.versionName = 'phoenix';
 
+  /// window.consoleが存在しない場合のダミー関数定義
   if (!window.console) {
       (function() {
           var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml", "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
